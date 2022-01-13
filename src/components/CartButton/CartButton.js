0a7f320a0cdbea0fbcb060dc2ab214b4239c744f
Sam Preston <sam.preston11@gmail.com>
2022-01-13 15:33:12 -0800

    Cart icon and badge added to cart button.

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -1,9 +1,14 @@
-import styles from './CartButton.module.css';
+import styles from "./CartButton.module.css";
+import CartIcon from "../CartIcon/CartIcon";
 
 const CartButton = (props) => {
-    return (
-        <button type='button' className={styles.button}>Your Cart</button>
-    );
+  return (
+    <button type="button" className={styles.button}>
+      <CartIcon className='icon'/>
+      <span>Your Cart</span>
+      <span className={styles.badge}>1</span>
+    </button>
+  );
 };
 
 export default CartButton;
