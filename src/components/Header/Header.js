b32e88d2481cee1c1c99f9cf34883a7437d2e43e
Sam Preston <sam.preston11@gmail.com>
2022-01-13 14:00:13 -0800

    Header component started.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+    return(
+        <div className={styles.header}>
+            <h1>Order Food</h1>
+        </div>
+    );
+};
+
+export default Header;
