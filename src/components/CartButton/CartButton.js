5b8a9398a4bb33167aca3ab55cd4e5ce52a5cde6
Sam Preston <sam.preston11@gmail.com>
2022-01-17 15:23:45 -0800

    Added bump animation to cart button.

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -1,17 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./CartButton.module.css";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../CartIcon/CartIcon";
 
 const CartButton = (props) => {
+
   const cartCtx = useContext(CartContext);
 
-  const numItems = cartCtx.items.reduce((curNumber, item) => {
+  const [buttonHighlight, setButtonHighlight] = useState(false);
+
+  const { items } = cartCtx;
+
+
+  const numItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0)
 
+
+  const buttonClasses = `${styles.button} ${buttonHighlight ? styles.bump: ''}`
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return
+    }
+    setButtonHighlight(true);
+
+    const timer = setTimeout(() => {
+      setButtonHighlight(false);
+    }, 300)
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items])
+
   return (
-    <button type="button" className={styles.button} onClick={props.onViewCart}>
+    <button type="button" className={buttonClasses} onClick={props.onViewCart}>
       <span className={styles.icon}><CartIcon /></span>
       <span>Your Cart</span>
       <span className={styles.badge}>{numItems}</span>
