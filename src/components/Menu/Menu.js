aceb381ced71f35cc24f9112f287285adba040ba
Sam Preston <sam.preston11@gmail.com>
2022-01-18 15:42:39 -0800

    Moved mealData to its own file - cleaned up Menu component.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,40 +1,13 @@
 import styles from "./Menu.module.css";
 import Card from "../UI/Card/Card";
 import MenuItem from "../MenuItem/MenuItem";
-
-// Menu selection data
-const dummyMealData = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { mealData } from "../../store/menu-data";
 
 // Displays all MenuItems to the user - passes the meal.id as key & all meal data as item to MenuItem
 const Menu = () => {
   return (
     <Card className={styles.menu}>
-      {dummyMealData.map((meal) => (
+      {mealData.map((meal) => (
         <MenuItem key={meal.id} item={meal} />
       ))}
     </Card>
