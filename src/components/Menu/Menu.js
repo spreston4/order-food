b01575141a8bcc4bfafc74357d7c32d6b9590f08
Sam Preston <sam.preston11@gmail.com>
2022-01-21 15:14:42 -0800

    Refactored to fetch meal data from a firebase backend.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,13 +1,39 @@
+import { useEffect, useState } from "react/cjs/react.development";
 import styles from "./Menu.module.css";
 import Card from "../UI/Card/Card";
 import MenuItem from "../MenuItem/MenuItem";
-import { mealData } from "../../store/menu-data";
 
 // Displays all MenuItems to the user - passes the meal.id as key & all meal data as item to MenuItem
 const Menu = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://order-food-9b59a-default-rtdb.firebaseio.com/meals.json"
+      );
+      const data = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
   return (
     <Card className={styles.menu}>
-      {mealData.map((meal) => (
+      {meals.map((meal) => (
         <MenuItem key={meal.id} item={meal} />
       ))}
     </Card>
