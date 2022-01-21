a67273289622008d5f40bc9a843bba9fd4940dce
Sam Preston <sam.preston11@gmail.com>
2022-01-21 15:33:05 -0800

    Added loading state formatting when fetching meal data.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,9 +6,12 @@ import MenuItem from "../MenuItem/MenuItem";
 // Displays all MenuItems to the user - passes the meal.id as key & all meal data as item to MenuItem
 const Menu = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
+
       const response = await fetch(
         "https://order-food-9b59a-default-rtdb.firebaseio.com/meals.json"
       );
@@ -26,6 +29,7 @@ const Menu = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
@@ -33,6 +37,9 @@ const Menu = () => {
 
   return (
     <Card className={styles.menu}>
+      <div className={styles.messages}>
+        {isLoading && <p>Meal choices are loading!</p>}
+      </div>
       {meals.map((meal) => (
         <MenuItem key={meal.id} item={meal} />
       ))}
