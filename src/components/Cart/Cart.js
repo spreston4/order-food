236102b212216078bec581d00f244ca6a8156cd7
Sam Preston <sam.preston11@gmail.com>
2022-01-24 15:42:37 -0800

    Added details to the order confirmation message.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,11 +12,14 @@ const Cart = (props) => {
   const [checkout, setCheckout] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [didSubmit, setDidSubmit] = useState(false);
+  const [customerData, setCustomerData] = useState({});
 
+  // Remove selected item from CartContext
   const removeItemHandler = (id) => {
     cartCtx.removeItem(id);
   };
 
+  // Add item to CartContext
   const addItemHandler = (item) => {
     cartCtx.addItem({
       id: item.id,
@@ -26,36 +29,49 @@ const Cart = (props) => {
     });
   };
 
+  // Reset CartContext
   const emptyCartHandler = () => {
     cartCtx.emptyCart();
   };
 
+  // Show Checkout form
   const orderButtonHandler = () => {
     setCheckout(true);
   };
 
+  // Close Checkout form
   const closeOrderHandler = () => {
     setCheckout(false);
   };
 
+  const closeOrderDetailsHandler = () => {
+    emptyCartHandler();
+    props.onCloseCart();
+  };
+
+  // Submit order to database when user confirms their order
   const submitOrderHandler = async (userData) => {
     setIsSubmitting(true);
+    setCustomerData(userData);
 
-    await fetch(
-      "https://order-food-9b59a-default-rtdb.firebaseio.com/orders.json",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          user: userData,
-          orderedItems: cartCtx.items,
-          totalAmount: totalAmount,
-        }),
-      }
-    );
+    try {
+      await fetch(
+        "https://order-food-9b59a-default-rtdb.firebaseio.com/orders.json",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            user: userData,
+            orderedItems: cartCtx.items,
+            totalAmount: totalAmount,
+          }),
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+    }
 
     setIsSubmitting(false);
     setDidSubmit(true);
-    emptyCartHandler();
   };
 
   // Ensure the price always diplays to 2 decimal places
@@ -116,11 +132,39 @@ const Cart = (props) => {
   // Content to return while order is submtting
   const isSubmittingContent = <p>Your order is processing</p>;
 
+  // Content to return after order has submitted
   const didSubmitContent = (
     <React.Fragment>
-      <p>Your order has been placed!</p>
+      <div className={styles.delivery}>
+        <h2>Your order has been placed!</h2>
+        <p>
+          Thank you for your order! However, this is a fictional restaurant.
+          Your order will be ready in approximately ∞ minutes.
+        </p>
+
+        <h3>Delivery Details</h3>
+        <p>{customerData.name}</p>
+        <p>
+          {customerData.phone} / {customerData.email}
+        </p>
+        <p>{customerData.address}</p>
+        <p>
+          {customerData.city}, {customerData.state} {customerData.zip}
+        </p>
+
+        <h3>Order Details</h3>
+        {cartCtx.items.map((item) => (
+          <p key={item.id}>
+            {item.name} x {item.amount}
+          </p>
+        ))}
+
+        <h3>Total</h3>
+        <p>{totalAmount}</p>
+      </div>
+
       <div className={styles.order}>
-        <Button onClick={props.onCloseCart} className={styles.alt}>
+        <Button onClick={closeOrderDetailsHandler} className={styles.alt}>
           Close
         </Button>
       </div>
